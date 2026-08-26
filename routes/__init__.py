e7d0f3f1f46b67ef3b66b963e9819644e9ce89d4
drg49 <dan.gavin49@gmail.com>
2026-08-26 13:28:12 -0700

    Remove recipe blueprint

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,11 +2,9 @@ from flask import Blueprint
 
 # Import the route handlers from other files
 from .authentication import authentication
-from .recipes import recipes
 
 # Create the blueprints
 routes = Blueprint('routes', __name__)
 
 # Register the blueprints with the Flask app
 routes.register_blueprint(authentication, url_prefix='/authentication')
-routes.register_blueprint(recipes, url_prefix='/recipes')
