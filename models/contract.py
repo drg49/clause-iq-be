a555670f732dab13dae70cf150341e77b4c80019
Danny Gavin <dan.gavin49@gmail.com>
2026-09-02 02:23:00 -0700

    Add contract field

---
+++ models/contract.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Contract(db.Model):
+    __tablename__ = "contracts"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Owner
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("users.id"),
+        nullable=False
+    )
+
+    # Contract information
+    name = db.Column(
+        db.String(255),
+        nullable=False
+    )
+
+    # S3
+    s3_key = db.Column(
+        db.String(500),
+        nullable=False
+    )
+
+    # Timestamp
+    created_at = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
